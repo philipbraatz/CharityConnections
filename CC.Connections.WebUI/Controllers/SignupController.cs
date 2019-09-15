13e379b0b840805f9bf97d6cee5cba974f386707
Keane Laux <300027473@fvtc.edu>
2019-09-15 16:08:37 -0500

    Added Signup Controller along with a Signup view Page(page isn't found)

---
+++ CC.Connections.WebUI/Controllers/SignupController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CC.Connections.WebUI.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: Signup
+        public ActionResult Create(string returnurl)
+        {
+            ViewBag.ReturnUrl = returnurl;
+            return View();
+        }
+    }
+}
