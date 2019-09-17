8c06fbd48b4472fff9747f8ffe0fb86082e5f47b
philip braatz <philblob@live.com>
2019-09-17 13:55:59 -0500

    Moved SignUp to the LoginController. (Working)

--- CC.Connections.PL/DBconnections.Designer.cs
+++ CC.Connections.PL/DBconnections.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\philb\Source\Repos\Charity Connections\CC.Connections.PL\DBconnections.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\philb\source\repos\Charity Connections\CC.Connections.PL\DBconnections.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
