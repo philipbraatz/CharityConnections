f2d2d99874abadc137b114837e36d3b8e7c0ad45
Keane Laux <300027473@fvtc.edu>
2019-10-08 17:48:38 -0500

    Added Account page (after logging in, click the account tab and you'll see your information)

---
+++ CC.Connections.WebUI/Controllers/VolunteerProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CC.Connections.BL;
+
+namespace CC.Connections.WebUI.Controllers
+{
+    public class VolunteerProfileController : Controller
+    {
+        // GET: VolunteerProfile
+        public ActionResult ProfileView(string id)
+        {
+            ViewBag.ReturnUrl = id;
+            ContactInfo c = new ContactInfo();
+            Password p = (Password)Session["member"];
+
+            return View(new ContactInfo(p.email));
+        }
+
+
+        
+    }
+}
