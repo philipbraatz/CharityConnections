f6bda5f281170b0ac5a69bfe50b914be03001d7d
philip braatz <philblob@live.com>
2019-11-18 19:23:30 -0600

    Added TimePicker_MVC project. TODO merge with WebUI

---
+++ TimePicker_MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimePicker_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string selectedTime)
+        {
+            ViewBag.Message = "Selected Time: " + selectedTime;
+            return View();
+        }
+    }
+}
---
+++ TimePicker_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimePicker_MVC.MvcApplication" Language="C#" %>
